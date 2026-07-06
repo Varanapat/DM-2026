a0adf6da3bfefca3d7583b354147320689cc7b82
Varanapat <varanapat3458@gmail.com>
2026-07-06 16:10:47 +0700

    draft 2
    Update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from 'node:path'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//       '@/components': path.resolve(__dirname, './src/components'),
+//       '@/pages': path.resolve(__dirname, './src/pages'),
+//       '@/data': path.resolve(__dirname, './src/data'),
+//       '@/styles': path.resolve(__dirname, './src/styles'),
+//     },
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'node:path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@/components': path.resolve(__dirname, './src/components'),
-      '@/pages': path.resolve(__dirname, './src/pages'),
-      '@/data': path.resolve(__dirname, './src/data'),
-      '@/styles': path.resolve(__dirname, './src/styles'),
-    },
-  },
+  base: '/DM-2026/',
 })
